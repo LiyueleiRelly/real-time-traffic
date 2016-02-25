472e21666749eaaf3cd1456fdac0c4b723376d4e
LiyueleiRelly <skyxihuan@vip.qq.com>
2016-02-25 20:47:58 +0800

    init

--- js/basefun.js
+++ js/basefun.js
@@ -137,7 +137,7 @@ function roaddraw(section){
     });
     polyline.setMap(map);
 }
-//添加省份坐标
+//添加省份
 function addprovince(){
     map.plugin(['AMap.DistrictSearch'],function(){
         var opts = {
